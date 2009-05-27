52eb2bcf2e360d2b636253776945ebe7dbc8a248
Benjamin Kampmann <benjamin.kampmann@prozentor.de>
2009-05-27 20:45:21 +0200

    connect to the mmkeys helper

--- mupnpkeys.py
+++ mupnpkeys.py
@@ -10,6 +10,8 @@ import os
 if __name__ == '__main__':
     from twisted.internet import gtk2reactor
     gtk2reactor.install()
+    import dbus.glib
+
 from coherence import log
 from coherence.upnp.core.utils import parse_xml, getPage, means_true
 
@@ -22,6 +24,12 @@ class MediaRendererClient(log.Loggable):
         self.muted = False
         self.device = None
 
+        for name in ['Pause', 'Stop', 'Previous', 'Next']:
+            setattr(self, name.lower(), self._proxy(name))
+
+        # play is a bit special
+        setattr(self, 'play', self._proxy('Play', Speed=1))
+
     def connect(self, device):
         assert self.device is None, """Don't connect as long as another device is still connected, stupid"""
         self.device = device
@@ -31,6 +39,14 @@ class MediaRendererClient(log.Loggable):
         service.subscribe_for_variable('Mute',
                 callback=lambda var: setattr(self, 'muted',
                         means_true(var.value)))
+
+    def _proxy(self, name, **kw):
+        def wrapped():
+            print name, kw
+            service = self.device.get_service_by_type('AVTransport')
+            action = service.get_action(name)
+            return action.call(InstanceID=0, **kw)
+        return wrapped
 
     def disconnect(self):
         pass
@@ -61,8 +77,13 @@ if __name__ == '__main__':
             client.connect(device)
         elif udn:
             client.connect(coherence.get_device_with_id(udn))
+        else:
+            return
+
+        from mmkeys import Mmkeys
+        keyer = Mmkeys(client)
 
     #reactor.callLater(0.2, client.connect, uid)
     ctp.connect(found_one, 'Coherence.UPnP.ControlPoint.MediaRenderer.detected')
-    task.LoopingCall(observe).start(1.0)
+    #task.LoopingCall(observe).start(1.0)
     reactor.run()

239e8249cf86ad65f89f901ca0f4f4e5d0e5acdb
Benjamin Kampmann <ben.kampmann@googlemail.com>
2009-06-12 16:59:04 +0200

    future code

--- upnpremotelet/media_renderer_client.py
+++ upnpremotelet/media_renderer_client.py
@@ -27,12 +27,29 @@ class MediaRendererClient(log.Loggable):
         assert self.device is None, """Don't connect as long as another device is still connected, stupid"""
         self.device = device
         print "set to", device
+        """
         service = self.device.get_service_by_type('RenderingControl')
         service.subscribe_for_variable('Volume',
                 callback=lambda var: setattr(self, 'volume', var.value))
         service.subscribe_for_variable('Mute',
                 callback=lambda var: setattr(self, 'muted',
                         means_true(var.value)))
+        service = self.device.get_service_by_type('AVTransport')
+        service.subscribe_for_variable('AVTransportURI',
+                callback=lambda var: setattr(self, 'uri', var.value))
+        service.subscribe_for_variable('CurrentTrackMetaData',
+                callback=lambda var: setattr(self, 'track_metadata', var.value))
+        service.subscribe_for_variable('TransportState',
+                callback=lambda var: setattr(self, 'state', var.value))
+        service.subscribe_for_variable('CurrentTransportActions',
+                callback=lambda var: setattr(self, 'actions', var.value))
+
+        service.subscribe_for_variable('AbsTime',
+                callback=lambda var: setattr(self, 'abstime', var.value))
+        service.subscribe_for_variable('TrackDuration',
+                callback=lambda var: setattr(self, 'duration', var.value))
+        """
+
 
     def _proxy(self, name, **kw):
         def wrapped():
